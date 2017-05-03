2dd6f659e728e60403cb15220ba4bd449652bc54
a1679380 <a1679380@student.adelaide.edu.au>
2017-05-03 21:31:27 +0930

    Search account from database to login

--- source/FindingTask/src/main/java/servlet/LoginServlet.java
+++ source/FindingTask/src/main/java/servlet/LoginServlet.java
@@ -9,7 +9,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -28,43 +30,63 @@ public class LoginServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
+		PrintWriter out = response.getWriter();
 		String userName = request.getParameter("username").trim();  
 	    String userPwd = request.getParameter("password").trim();  
 	    HttpSession session = request.getSession();
-	    session.setAttribute("myname", userName);
-	    session.setAttribute("mypwd", userPwd);
-	    session.setAttribute("name", "111");
-	    session.setAttribute("pwd", "222");
-	    if(userName.equals("111") && userPwd.equals("222")) {  
-	    	List<String> database = new ArrayList<String>();
-	        try {
-				Connection conn = getConnection();
-		    	Statement state = conn.createStatement();
-		    	String sql = "SELECT * FROM extraction";
-		    	ResultSet tempData = state.executeQuery(sql);
-		    	while(tempData.next()){
-		    		//String data = "";
-		    		for(int i=1;i<10;i++){
-		    			//data += tempData.getString(i) + " ";
-		    			database.add(tempData.getString(i));
-		    		}
-		    		//database.add(data);
-		    	}
-		    	conn.close();
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-	        //System.out.println("data: "+database);
-	        session.setAttribute("data", database);
-	        response.sendRedirect("database.jsp");
-	    } 
-	    else {  
-	        //response.sendRedirect("login.jsp");   
-	    	PrintWriter out = response.getWriter();
-	    	out.println("username or password error");
+	    Map<String,String> account = getAccount();
+	    if(account.containsKey(userName)){
+	    	String pwd = account.get(userName);
+	    	if(userPwd.equals(pwd)) {  
+		    	List<String> database = new ArrayList<String>();
+		        try {
+					Connection conn = getConnection();
+			    	Statement state = conn.createStatement();
+			    	String sql = "SELECT * FROM extraction";
+			    	ResultSet tempData = state.executeQuery(sql);
+			    	while(tempData.next()){
+			    		for(int i=1;i<10;i++){
+			    			database.add(tempData.getString(i));
+			    		}	
+			    	}
+			    	conn.close();
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		        //System.out.println("data: "+database);
+		        session.setAttribute("data", database);
+		        response.sendRedirect("database.jsp");
+		    } 
+		    else {  
+		    	out.println("password is wrong, try again");
+		    }
+	    }else{
+	    	out.println("username does not exist");
 	    }
+	    
+	}
+	
+	private Map<String,String> getAccount(){
+		Map<String,String> account = new HashMap<String,String>();
+		try {
+			Connection conn = getConnection();
+	    	Statement state = conn.createStatement();
+	    	String sql = "SELECT * FROM account";
+	    	ResultSet tempData = state.executeQuery(sql);
+	    	while(tempData.next()){
+	    		String key = tempData.getString(1);
+	    		String value = tempData.getString(2);
+	    		account.put(key, value);
+	    	}
+	    	conn.close();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return account;
 	}
 	
 	private static Connection getConnection() throws URISyntaxException, SQLException {
